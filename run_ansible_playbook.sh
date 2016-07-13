8593135ce69d5250fbd7e7c93392f810b3a29382
ronost <ronost81@gmail.com>
2016-07-13 23:37:40 +0200

    Changed from using lineinfile to mount module.
    Also stopped relying on UUID because of some strange race condition when
    trying to obtain current UUID. Using device names is good enough.

--- run_ansible_playbook.sh
+++ run_ansible_playbook.sh
@@ -1,1 +1,1 @@
-ansible-playbook provision.yml -i hosts --vault-password-file ~/.vault_password_pifarm.txt -vvvv
+ansible-playbook provision.yml -i hosts --vault-password-file ~/.vault_password_pifarm.txt -vvvv 
