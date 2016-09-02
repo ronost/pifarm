31d919de8bfb7221496cd32d1855c8f80d6d4497
ronost <ronost81@gmail.com>
2016-09-02 22:54:18 +0200

    Added exclude patterns to backup.sh, changed log output.

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -1,10 +1,10 @@
-log=/home/pi/backup.log
-echo "Backup source \"ronny\" start: $(date +%Y-%m-%d" "%H:%M:%S)" >> $log
-rsync -av /mnt/backup_source_ronny/ /mnt/backup_destination/backup/ronny/ >> $log
-echo "Backup source \"ronny\" end: $(date +%Y-%m-%d" "%H:%M:%S)" >> $log
-echo "---------------------------------------------------" >> $log
-echo "Backup source \"bella\" start: $(date +%Y-%m-%d" "%H:%M:%S)" >> $log
-rsync -av /mnt/backup_source_bella/ /mnt/backup_destination/backup/bella/ >> $log
-echo "Backup source \"bella\" end: $(date +%Y-%m-%d" "%H:%M:%S)\n\n" >> $log
-df -h -t cifs >> $log
-echo "===================================================" >> $log
+#!/bin/bash
+echo "Backup source \"ronny\" start: $(date +%Y-%m-%d" "%H:%M:%S)"
+rsync -av --exclude={".*",".*/"} /mnt/backup_source_ronny/ /mnt/backup_destination/backup/ronny/
+echo "Backup source \"ronny\" end: $(date +%Y-%m-%d" "%H:%M:%S)"
+echo "---------------------------------------------------"
+echo "Backup source \"bella\" start: $(date +%Y-%m-%d" "%H:%M:%S)"
+rsync -av --exclude={".*",".*/"} /mnt/backup_source_bella/ /mnt/backup_destination/backup/bella/
+echo "Backup source \"bella\" end: $(date +%Y-%m-%d" "%H:%M:%S)\n\n"
+df -h -t cifs
+echo "==================================================="
