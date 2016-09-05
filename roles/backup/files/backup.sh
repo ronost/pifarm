81a98d2ffa1ba2494e90445383b540851df285b3
ronost <ronost81@gmail.com>
2016-09-05 22:40:27 +0200

    Remove verbose output from rsync backup comman

--- roles/backup/files/backup.sh
+++ roles/backup/files/backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Backup source \"ronny\" start: $(date +%Y-%m-%d" "%H:%M:%S)"
-rsync -av --size-only --exclude={".*",".*/","*.CR2"} /mnt/backup_source_ronny/ /mnt/backup_destination/backup/ronny/
+rsync -a --size-only --exclude={".*",".*/","*.CR2"} /mnt/backup_source_ronny/ /mnt/backup_destination/backup/ronny/
 echo "Backup source \"ronny\" end: $(date +%Y-%m-%d" "%H:%M:%S)"
 echo "---------------------------------------------------"
 echo "Backup source \"bella\" start: $(date +%Y-%m-%d" "%H:%M:%S)"
-rsync -av --size-only --exclude={".*",".*/","*.CR2"} /mnt/backup_source_bella/ /mnt/backup_destination/backup/bella/
+rsync -a --size-only --exclude={".*",".*/","*.CR2"} /mnt/backup_source_bella/ /mnt/backup_destination/backup/bella/
 echo "Backup source \"bella\" end: $(date +%Y-%m-%d" "%H:%M:%S)\n\n"
 df -h -t cifs
 echo "==================================================="
